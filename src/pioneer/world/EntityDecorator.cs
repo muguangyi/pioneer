ecb116226256a3775fef33e7d5124e542e6a655c
mouguangyi <mouguangyi@ztgame.com>
2019-10-22 12:11:13 +0800

    Add entity decorator.

---
+++ src/pioneer/world/EntityDecorator.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * This file is part of the Pioneer package.
+ *
+ * (c) MuGuangYi <muguangyi@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Pioneer
+{
+    sealed class EntityDecorator : IEntityDecorator, IDisposable
+    {
+        private Dictionary<string, Action<IEntity>> templates = new Dictionary<string, Action<IEntity>>();
+
+        public void Dispose()
+        { }
+
+        public bool AddTemplate(string template, Action<IEntity> decorator)
+        {
+            if (this.templates.ContainsKey(template))
+            {
+                return false;
+            }
+
+            this.templates.Add(template, decorator);
+            return true;
+        }
+
+        public void Apply(string template, IEntity entity)
+        {
+            if (string.IsNullOrEmpty(template))
+            {
+                return;
+            }
+
+            if (this.templates.TryGetValue(template, out Action<IEntity> decorator))
+            {
+                decorator(entity);
+            }
+        }
+    }
+}
---
+++ src/pioneer/world/IEntityDecorator.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * This file is part of the Pioneer package.
+ *
+ * (c) MuGuangYi <muguangyi@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pioneer
+{
+    /// <summary>
+    /// Entity decorator interface.
+    /// </summary>
+    interface IEntityDecorator
+    {
+        /// <summary>
+        /// Apply template to target entity.
+        /// </summary>
+        /// <param name="template">Template name.</param>
+        /// <param name="entity">Target entity instance.</param>
+        void Apply(string template, IEntity entity);
+    }
+}
